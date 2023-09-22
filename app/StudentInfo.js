ae59cec40a2a193bd5e267fcc3152dddac991e12
Vincemanio <john.manio@edu.sait.ca>
2023-09-22 10:28:47 -0600

    week 2 assignment

---
+++ app/StudentInfo.js
@@ -0,0 +1,13 @@
+import link from 'next/link'
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <div>
+                <p>Name: John Vincent Manio</p>
+                <p> Course Section: CPRG 306 A</p>
+                <p><a href="https://github.com/Vincemanio">https://github.com/Vincemanio</a></p>
+            </div>
+        </main>
+    );
+}
