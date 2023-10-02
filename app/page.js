4c25338e4a4e688d2a62e5e05791a046e2de4005
Vincemanio <john.manio@edu.sait.ca>
2023-10-02 16:53:05 -0600

    week 3

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
       <h1>CPRG 306: Web Development 2 Assignment </h1>
       <StudentInfo/>
       <p><Link href="/week2">Week 2</Link></p>
+      <p><Link href="/week3">Week 3</Link></p>
     </main>
   )
 }
---
+++ app/week3/page.js
@@ -0,0 +1,13 @@
+import Itemlist from "./item-list";
+import Item from "./item";
+
+function Page(){
+    return(
+        <main className="p-2">
+            <h2 className="text4x1 font-bold mb-4">Shopping Liist</h2>
+            <Itemlist/>
+
+        </main>
+    )
+}
+export default Page;
