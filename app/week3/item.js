743e3b1b52b2e57863c4330344ecd7bd4e2cc346
Vincemanio <john.manio@edu.sait.ca>
2023-10-09 22:52:07 -0600

    Week 4 assignment

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,6 +1,6 @@
 export default function Item({ name, quantity, category }) {
     return (
-      <ol className="p-5 m-5 shadow-md rounded-lg mb-4 bg-slate-700 max-w-sm">
+    <ol className="p-5 m-5 shadow-md rounded-lg mb-4 bg-slate-700 max-w-sm">
         <li>Name: {name}</li>
         <li>Quantity: {quantity}</li>
         <li>Category: {category}</li>
